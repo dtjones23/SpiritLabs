c93df9bc6819bc9de24fd79ca3fcdcb8d39b9ab4
dtjones23 <142169871+dtjones23@users.noreply.github.com>
2025-03-01 13:40:08 -0600

    adjusted config file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/spiritlabs');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/spiritlabs');
 
 module.exports = mongoose.connection;
